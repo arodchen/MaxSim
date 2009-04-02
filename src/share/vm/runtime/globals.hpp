c464f1fe0fd7bda92a7e00be22c5b3a807969649
apetrusenko <apetrusenko@localhost>
2009-04-02 05:22:02 -0700

    Merge

--- src/share/vm/runtime/globals.hpp
+++ src/share/vm/runtime/globals.hpp
@@ -47,6 +47,7 @@ define_pd_global(intx, Tier4BackEdgeThreshold,       0);
 define_pd_global(intx, OnStackReplacePercentage,     0);
 define_pd_global(bool, ResizeTLAB,                   false);
 define_pd_global(intx, FreqInlineSize,               0);
+define_pd_global(intx, InlineSmallCode,              0);
 define_pd_global(intx, NewSizeThreadIncrease,        4*K);
 define_pd_global(intx, NewRatio,                     4);
 define_pd_global(intx, InlineClassNatives,           true);
@@ -1315,9 +1316,9 @@ class CommandLineFlags {
                                                                             \
   product(intx, ParGCArrayScanChunk, 50,                                    \
           "Scan a subset and push remainder, if array is bigger than this") \
-                                                                            \
   product(bool, ParGCTrimOverflow, true,                                    \
           "Eagerly trim the overflow lists (useful for UseCompressedOops")  \
+                                                                            \
                                                                             \
   notproduct(bool, ParGCWorkQueueOverflowALot, false,                       \
           "Whether we should simulate work queue overflow in ParNew")       \
@@ -2619,7 +2620,7 @@ class CommandLineFlags {
   develop(intx, MaxRecursiveInlineLevel, 1,                                 \
           "maximum number of nested recursive calls that are inlined")      \
                                                                             \
-  product(intx, InlineSmallCode, 1000,                                      \
+  product_pd(intx, InlineSmallCode,                                         \
           "Only inline already compiled methods if their code size is "     \
           "less than this")                                                 \
                                                                             \
