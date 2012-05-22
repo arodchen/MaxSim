1c0c8311a4f9807ac82118d2ea485160499bb742
Doug Simon <doug.simon@oracle.com>
2012-05-22 16:42:56 +0200

    correctly handle null for replacements parameter in  Graph.addDuplicates()

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/FixedGuardNode.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes;
+
+import com.oracle.graal.cri.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+import com.oracle.max.cri.ri.*;
+
+public final class FixedGuardNode extends FixedWithNextNode implements Simplifiable, Lowerable, LIRLowerable, Node.IterableNodeType {
+
+    @Input private final NodeInputList<BooleanNode> conditions;
+    private final RiDeoptReason deoptReason;
+    private final RiDeoptAction action;
+    private final long leafGraphId;
+
+    public FixedGuardNode(BooleanNode condition, RiDeoptReason deoptReason, RiDeoptAction action, long leafGraphId) {
+        super(StampFactory.illegal());
+        this.action = action;
+        this.leafGraphId = leafGraphId;
+        this.conditions = new NodeInputList<>(this, new BooleanNode[] {condition});
+        this.deoptReason = deoptReason;
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool gen) {
+        for (BooleanNode condition : conditions()) {
+            gen.emitGuardCheck(condition, deoptReason, action, leafGraphId);
+        }
+    }
+
+    public void addCondition(BooleanNode x) {
+        conditions.add(x);
+    }
+
+    public NodeInputList<BooleanNode> conditions() {
+        return conditions;
+    }
+
+    @Override
+    public void simplify(SimplifierTool tool) {
+        for (BooleanNode n : conditions.snapshot()) {
+            if (n instanceof ConstantNode) {
+                ConstantNode c = (ConstantNode) n;
+                if (c.asConstant().asBoolean()) {
+                    conditions.remove(n);
+                } else {
+                    FixedNode next = this.next();
+                    if (next != null) {
+                        tool.deleteBranch(next);
+                    }
+                    setNext(graph().add(new DeoptimizeNode(RiDeoptAction.InvalidateRecompile, deoptReason, leafGraphId)));
+                    return;
+                }
+            }
+        }
+        if (conditions.isEmpty()) {
+            ((StructuredGraph) graph()).removeFixed(this);
+        }
+    }
+
+    @Override
+    public void lower(CiLoweringTool tool) {
+        AnchorNode newAnchor = graph().add(new AnchorNode());
+        for (BooleanNode b : conditions) {
+            newAnchor.dependencies().add(tool.createGuard(b, deoptReason, action, leafGraphId));
+        }
+        ((StructuredGraph) graph()).replaceFixedWithFixed(this, newAnchor);
+    }
+}
---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/ValueAnchorNode.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.extended;
+
+import com.oracle.graal.graph.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.calc.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+import com.oracle.max.cri.ci.*;
+
+/**
+ * The ValueAnchor instruction keeps non-CFG (floating) nodes above a certain point in the graph.
+ */
+
+public final class ValueAnchorNode extends FixedWithNextNode implements Canonicalizable, LIRLowerable, Node.IterableNodeType {
+
+    public ValueAnchorNode(ValueNode... values) {
+        super(StampFactory.illegal(), values);
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool gen) {
+        // Nothing to emit, since this node is used for structural purposes only.
+    }
+
+    public void addAnchoredValue(ValueNode value) {
+        this.dependencies().add(value);
+    }
+
+    @Override
+    public ValueNode canonical(CanonicalizerTool tool) {
+        if (this.predecessor() instanceof ValueAnchorNode) {
+            // transfer values and remove
+            ValueAnchorNode previousAnchor = (ValueAnchorNode) this.predecessor();
+            for (Node node : dependencies().nonNull()) {
+                previousAnchor.dependencies().add(node);
+            }
+            return null;
+        }
+        for (Node node : dependencies().nonNull()) {
+            if (node instanceof ConstantNode) {
+                continue;
+            }
+            if (node instanceof IntegerDivNode || node instanceof IntegerRemNode) {
+                ArithmeticNode arithmeticNode = (ArithmeticNode) node;
+                if (arithmeticNode.y().isConstant()) {
+                    CiConstant  constant = arithmeticNode.y().asConstant();
+                    assert constant.kind == arithmeticNode.kind() : constant.kind + " != " + arithmeticNode.kind();
+                    if (constant.asLong() != 0) {
+                        continue;
+                    }
+                }
+            }
+            return this; // still necessary
+        }
+        return null; // no node which require an anchor found
+    }
+}
