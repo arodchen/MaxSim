651ce09bb5d1f39a234c4f02223e5418fd0799ae
Gilles Duboscq <duboscq@ssw.jku.at>
2012-09-04 17:09:46 +0200

    IGV : Add idx to binary graphs to help visualizing nodes during comparison

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/java/InstanceOfNode.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.java;
+
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.calc.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+
+/**
+ * The {@code InstanceOfNode} represents an instanceof test.
+ */
+public final class InstanceOfNode extends BooleanNode implements Canonicalizable, Lowerable, LIRLowerable {
+
+    @Input private ValueNode object;
+    @Input private ValueNode targetClassInstruction;
+    private final ResolvedJavaType targetClass;
+    private final JavaTypeProfile profile;
+
+    /**
+     * Constructs a new InstanceOfNode.
+     *
+     * @param targetClassInstruction the instruction which produces the target class of the instanceof check
+     * @param targetClass the class which is the target of the instanceof check
+     * @param object the instruction producing the object input to this instruction
+     */
+    public InstanceOfNode(ValueNode targetClassInstruction, ResolvedJavaType targetClass, ValueNode object) {
+        this(targetClassInstruction, targetClass, object, null);
+    }
+
+    public InstanceOfNode(ValueNode targetClassInstruction, ResolvedJavaType targetClass, ValueNode object, JavaTypeProfile profile) {
+        super(StampFactory.condition());
+        this.targetClassInstruction = targetClassInstruction;
+        this.targetClass = targetClass;
+        this.object = object;
+        this.profile = profile;
+        assert targetClass != null;
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool gen) {
+    }
+
+    @Override
+    public void lower(LoweringTool tool) {
+        tool.getRuntime().lower(this, tool);
+    }
+
+    @Override
+    public ValueNode canonical(CanonicalizerTool tool) {
+        assert object() != null : this;
+
+        ObjectStamp stamp = object().objectStamp();
+        ResolvedJavaType type = stamp.type();
+
+        if (stamp.isExactType()) {
+            boolean subType = type.isSubtypeOf(targetClass());
+
+            if (subType) {
+                if (stamp.nonNull()) {
+                    // the instanceOf matches, so return true
+                    return ConstantNode.forBoolean(true, graph());
+                } else {
+                    // the instanceof matches if the object is non-null, so return true depending on the null-ness.
+                    negateUsages();
+                    return graph().unique(new IsNullNode(object()));
+                }
+            } else {
+                // since this type check failed for an exact type we know that it can never succeed at run time.
+                // we also don't care about null values, since they will also make the check fail.
+                return ConstantNode.forBoolean(false, graph());
+            }
+        } else if (type != null) {
+            boolean subType = type.isSubtypeOf(targetClass());
+
+            if (subType) {
+                if (stamp.nonNull()) {
+                    // the instanceOf matches, so return true
+                    return ConstantNode.forBoolean(true, graph());
+                } else {
+                    // the instanceof matches if the object is non-null, so return true depending on the null-ness.
+                    negateUsages();
+                    return graph().unique(new IsNullNode(object()));
+                }
+            } else {
+                // since the subtype comparison was only performed on a declared type we don't really know if it might be true at run time...
+            }
+        }
+        if (object().objectStamp().alwaysNull()) {
+            return ConstantNode.forBoolean(false, graph());
+        }
+        return this;
+    }
+
+    public ValueNode object() {
+        return object;
+    }
+
+    public ValueNode targetClassInstruction() {
+        return targetClassInstruction;
+    }
+
+    /**
+     * Gets the target class, i.e. the class being cast to, or the class being tested against.
+     * @return the target class
+     */
+    public ResolvedJavaType targetClass() {
+        return targetClass;
+    }
+
+    public JavaTypeProfile profile() {
+        return profile;
+    }
+}
---
+++ src/share/vm/graal/graalVMToCompiler.hpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+#ifndef SHARE_VM_GRAAL_GRAAL_VM_TO_COMPILER_HPP
+#define SHARE_VM_GRAAL_GRAAL_VM_TO_COMPILER_HPP
+
+#include "memory/allocation.hpp"
+#include "oops/oop.hpp"
+#include "runtime/handles.hpp"
+#include "runtime/thread.hpp"
+#include "classfile/javaClasses.hpp"
+#include "runtime/jniHandles.hpp"
+#include "runtime/javaCalls.hpp"
+
+class VMToCompiler : public AllStatic {
+
+private:
+  static jobject _compilerPermObject;
+  static jobject _vmToCompilerPermObject;
+  static jobject _vmToCompilerPermKlass;
+
+  static KlassHandle vmToCompilerKlass();
+  static Handle instance();
+
+public:
+  static Handle compilerInstance();
+
+  static jobject compilerPermObject() {
+    return _compilerPermObject;
+  }
+
+  // public static boolean HotSpotOptions.setOption(String option);
+  static jboolean setOption(Handle option);
+
+  // public static void HotSpotOptions.setDefaultOptions();
+  static void setDefaultOptions();
+
+  // public abstract boolean compileMethod(long vmId, String name, int entry_bci, boolean blocking);
+  static jboolean compileMethod(Handle hotspot_method, int entry_bci, jboolean blocking, int priority);
+
+  // public abstract void shutdownCompiler();
+  static void shutdownCompiler();
+  
+  // public abstract void startCompiler();
+  static void startCompiler();
+  
+  // public abstract void bootstrap();
+  static void bootstrap();
+
+  // public abstract JavaMethod createResolvedJavaMethod(long vmId, String name);
+  static oop createResolvedJavaMethod(jlong vmId, Handle name, TRAPS);
+
+  // public abstract JavaMethod createJavaMethod(String name, String signature, JavaType holder);
+  static oop createJavaMethod(Handle name, Handle signature, Handle holder, TRAPS);
+
+  // public abstract JavaField createJavaField(JavaType holder, String name, JavaType type, int flags, int offset);
+  static oop createJavaField(Handle holder, Handle name, Handle type, int index, int flags, TRAPS);
+
+  // public abstract JavaType createJavaType(String name);
+  static oop createJavaType(Handle name, TRAPS);
+
+  // public abstract JavaType createPrimitiveJavaType(int basicType);
+  static oop createPrimitiveJavaType(int basicType, TRAPS);
+
+  // public abstract Signature createSignature(String signature);
+  static oop createSignature(Handle name, TRAPS);
+
+  // public abstract Constant createConstant(Kind kind, long value);
+  static oop createConstant(Handle kind, jlong value, TRAPS);
+
+  // public abstract Constant createConstantFloat(float value);
+  static oop createConstantFloat(jfloat value, TRAPS);
+
+  // public abstract Constant createConstantDouble(double value);
+  static oop createConstantDouble(jdouble value, TRAPS);
+
+  // public abstract Constant createConstantObject(long vmId);
+  static oop createConstantObject(Handle object, TRAPS);
+};
+
+inline void check_pending_exception(const char* message, bool dump_core = false) {
+  Thread* THREAD = Thread::current();
+  if (THREAD->has_pending_exception()) {
+    Handle exception = PENDING_EXCEPTION;
+    CLEAR_PENDING_EXCEPTION;
+
+    assert(exception->is_a(SystemDictionary::Throwable_klass()), "Throwable instance expected");
+    JavaValue result(T_VOID);
+    JavaCalls::call_virtual(&result,
+                            exception,
+                            KlassHandle(THREAD,
+                            SystemDictionary::Throwable_klass()),
+                            vmSymbols::printStackTrace_name(),
+                            vmSymbols::void_method_signature(),
+                            THREAD);
+
+    vm_abort(dump_core);
+  }
+}
+
+inline void check_not_null(void* value, const char* message, bool dump_core = false) {
+  if (value == NULL) {
+    tty->print_cr("%s", message);
+    vm_abort(dump_core);
+  }
+}
+
+#endif // SHARE_VM_GRAAL_GRAAL_VM_TO_COMPILER_HPP
