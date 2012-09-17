1507bd2cb769787ee0e563a5f9962b2c91b94f53
Doug Simon <doug.simon@oracle.com>
2012-09-17 18:12:02 +0200

    added support for placing breakpoints in snippets

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/ReadNode.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.extended;
+
+import com.oracle.graal.api.code.*;
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+
+/**
+ * Reads an {@linkplain AccessNode accessed} value.
+ */
+public final class ReadNode extends AccessNode implements Node.IterableNodeType, LIRLowerable, Simplifiable/*, Canonicalizable*/ {
+
+    public ReadNode(ValueNode object, ValueNode location, Stamp stamp) {
+        super(object, location, stamp);
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool gen) {
+        gen.setResult(this, gen.emitLoad(gen.makeAddress(location(), object()), getNullCheck()));
+    }
+
+    // Canonicalization disabled until we have a solution for non-Object oops in Hotspot
+    /*@Override
+    public ValueNode canonical(CanonicalizerTool tool) {
+        return canonicalizeRead(this, tool);
+    }*/
+
+    public static ValueNode canonicalizeRead(Access read, CanonicalizerTool tool) {
+        MetaAccessProvider runtime = tool.runtime();
+        if (runtime != null && read.object() != null && read.object().isConstant() && read.object().kind() == Kind.Object) {
+            if (read.location().locationIdentity() == LocationNode.FINAL_LOCATION && read.location().getClass() == LocationNode.class) {
+                Object value = read.object().asConstant().asObject();
+                long displacement = read.location().displacement();
+                Kind kind = read.location().getValueKind();
+                Constant constant = kind.readUnsafeConstant(value, displacement);
+                if (constant != null) {
+                    System.out.println("Canon read to " + constant);
+                    return ConstantNode.forConstant(constant, runtime, read.node().graph());
+                }
+            }
+        }
+        return (ValueNode) read;
+    }
+
+    @Override
+    public void simplify(SimplifierTool tool) {
+        if (object().isConstant() && object().asConstant().isNull()) {
+            FixedNode successor = next();
+            tool.deleteBranch(successor);
+            if (isAlive()) {
+                replaceAtPredecessor(graph().add(new DeoptimizeNode(DeoptimizationAction.InvalidateReprofile, DeoptimizationReason.NullCheckException)));
+                safeDelete();
+            }
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private ReadNode(ValueNode object, ValueNode location) {
+        this(object, location, StampFactory.forNodeIntrinsic());
+    }
+
+    @SuppressWarnings("unused")
+    @NodeIntrinsic
+    public static <T> T readMemory(Object object, Object location) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/java/NewInstanceNode.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.java;
+
+import java.util.*;
+
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+import com.oracle.graal.nodes.virtual.*;
+
+/**
+ * The {@code NewInstanceNode} represents the allocation of an instance class object.
+ */
+@NodeInfo(nameTemplate = "New {p#instanceClass/s}")
+public final class NewInstanceNode extends FixedWithNextNode implements EscapeAnalyzable, Lowerable, LIRLowerable, Node.IterableNodeType {
+
+    private final ResolvedJavaType instanceClass;
+    private final boolean fillContents;
+
+    /**
+     * Constructs a NewInstanceNode.
+     *
+     * @param type the class being allocated
+     */
+    public NewInstanceNode(ResolvedJavaType type, boolean fillContents) {
+        super(StampFactory.exactNonNull(type));
+        this.instanceClass = type;
+        this.fillContents = fillContents;
+    }
+
+    /**
+     * Gets the instance class being allocated by this node.
+     *
+     * @return the instance class allocated
+     */
+    public ResolvedJavaType instanceClass() {
+        return instanceClass;
+    }
+
+    public boolean fillContents() {
+        return fillContents;
+    }
+
+    @Override
+    public void lower(LoweringTool tool) {
+        tool.getRuntime().lower(this, tool);
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool gen) {
+        gen.visitNewInstance(this);
+    }
+
+    private void fillEscapeFields(ResolvedJavaType type, List<ResolvedJavaField> escapeFields) {
+        if (type != null) {
+            fillEscapeFields(type.superType(), escapeFields);
+            for (ResolvedJavaField field : type.declaredFields()) {
+                escapeFields.add(field);
+            }
+        }
+    }
+
+    @Override
+    public EscapeOp getEscapeOp() {
+        if (instanceClass != null) {
+            assert !instanceClass().isArrayClass();
+            List<ResolvedJavaField> escapeFields = new ArrayList<>();
+            fillEscapeFields(instanceClass(), escapeFields);
+            final ResolvedJavaField[] fields = escapeFields.toArray(new ResolvedJavaField[escapeFields.size()]);
+            return new EscapeOp() {
+
+                @Override
+                public ValueNode[] fieldState() {
+                    ValueNode[] state = new ValueNode[fields.length];
+                    for (int i = 0; i < state.length; i++) {
+                        state[i] = ConstantNode.defaultForKind(fields[i].type().kind(), graph());
+                    }
+                    return state;
+                }
+
+                @Override
+                public VirtualObjectNode virtualObject(int virtualId) {
+                    return new VirtualInstanceNode(virtualId, instanceClass(), fields);
+                }
+            };
+        }
+        return null;
+    }
+}
