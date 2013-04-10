e9315514a9d631a0484e51a3571690f1ec5f98b4
Lukas Stadler <lukas.stadler@jku.at>
2013-04-10 10:17:35 +0200

    PEA: cleanup and documentation for recent changes

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/FixedWithNextNode.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2009, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes;
+
+import java.util.*;
+
+import com.oracle.graal.nodes.type.*;
+
+/**
+ * Base class of all nodes that are fixed within the control flow graph and have an immediate
+ * successor.
+ */
+public abstract class FixedWithNextNode extends FixedNode {
+
+    public FixedNode next() {
+        assert scheduledNext() == null || scheduledNext() instanceof FixedNode : "next() cannot be used while the graph is scheduled";
+        return (FixedNode) scheduledNext();
+    }
+
+    public void setNext(FixedNode x) {
+        setScheduledNext(x);
+    }
+
+    public FixedWithNextNode(Stamp stamp) {
+        super(stamp);
+    }
+
+    public FixedWithNextNode(Stamp stamp, List<ValueNode> dependencies) {
+        super(stamp, dependencies);
+    }
+
+    public FixedWithNextNode(Stamp stamp, ValueNode... dependencies) {
+        super(stamp, dependencies);
+    }
+}
