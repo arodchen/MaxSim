24a6bd573522b4bb238f7a0a4f3235916fc9d3f2
Andrey Rodchenko <andrey.rodchenko@manchester.ac.uk>
2017-03-03 00:53:06 +0000

    Added MaxSimPlatform.

---
+++ maxine/com.oracle.max.vm/src/com/sun/max/vm/maxsim/MaxSimPlatform.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 by Andrey Rodchenko, School of Computer Science,
+ * The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.sun.max.vm.maxsim;
+
+public class MaxSimPlatform {
+
+    /**
+     * Instructs MaxSim to exit fast forwarding mode.
+     */
+    public static synchronized void exitMaxSimFastForwardingMode() {
+        MaxSimMediator.exitZSimFastForwardingMode();
+        MaxSimMediator.dumpEventualStats(
+            MaxSimInterface.MaxineVMOperationMode.MAXINE_VM_OPERATION_MODE_RUNNING_NON_GC_VALUE);
+    }
+
+    /**
+     * Instructs MaxSim to enter fast forwarding mode.
+     */
+    public static synchronized void enterMaxSimFastForwardingMode() {
+        MaxSimMediator.dumpEventualStats(
+            MaxSimInterface.MaxineVMOperationMode.MAXINE_VM_OPERATION_MODE_UNKNOWN_VALUE);
+        MaxSimMediator.enterZSimFastForwardingMode();
+    }
+}
