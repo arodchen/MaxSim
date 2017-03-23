fa34766fa6d92f211fede0a7256ef5f87d2ebe72
Andrey Rodchenko <andrey.rodchenko@manchester.ac.uk>
2017-03-23 19:32:40 +0000

    Implemented MaxSim tagging.

--- maxine/com.oracle.max.vm/src/com/sun/max/unsafe/UnsafeCast.java
+++ maxine/com.oracle.max.vm/src/com/sun/max/unsafe/UnsafeCast.java
@@ -91,6 +91,7 @@ public final class UnsafeCast {
     @INTRINSIC(UNSAFE_CAST) public static short                    asShort(char value) { return (short) value; }
     @INTRINSIC(UNSAFE_CAST) public static long                     asLong(short value) { return (long) value; }
     @INTRINSIC(UNSAFE_CAST) public static short                    asShort(long value) { return (short) value; }
+    @INTRINSIC(UNSAFE_CAST) public static int                      asInt(short value) { return (int) value; }
 
     @INTRINSIC(UNSAFE_CAST) public static CodePointer              asCodePointer(long value) { return CodePointer.from(value); }
     @INTRINSIC(UNSAFE_CAST) public static CodePointer              asCodePointerTagged(long value) { return CodePointer.fromTaggedLong(value); }
--- maxine/com.oracle.max.vm/src/com/sun/max/vm/maxsim/MaxSimTaggingScheme.java
+++ maxine/com.oracle.max.vm/src/com/sun/max/vm/maxsim/MaxSimTaggingScheme.java
@@ -19,14 +19,28 @@
  */
 package com.sun.max.vm.maxsim;
 
+import com.oracle.max.criutils.TTY;
 import com.sun.max.annotate.INLINE;
 import com.sun.max.unsafe.*;
+import com.sun.max.vm.Log;
 import com.sun.max.vm.MaxineVM;
+import com.sun.max.vm.VMConfiguration;
+import com.sun.max.vm.VMOptions;
 import com.sun.max.vm.actor.holder.*;
+import com.sun.max.vm.code.Code;
+import com.sun.max.vm.heap.*;
 import com.sun.max.vm.layout.Layout;
+import com.sun.max.vm.layout.SpecificLayout;
 import com.sun.max.vm.object.ObjectAccess;
 import com.sun.max.vm.reference.Reference;
 import com.sun.max.vm.runtime.FatalError;
+import com.sun.max.vm.runtime.VmOperation;
+import com.sun.max.vm.stack.StackReferenceMapPreparer;
+import com.sun.max.vm.thread.VmThread;
+import com.sun.max.vm.thread.VmThreadLocal;
+import com.sun.max.vm.type.ClassRegistry;
+
+import static com.sun.max.vm.thread.VmThreadLocal.LOWEST_ACTIVE_STACK_SLOT_ADDRESS;
 
 /**
  * MaxSim (un)tagging scheme.
@@ -34,15 +48,223 @@ import com.sun.max.vm.runtime.FatalError;
 public class MaxSimTaggingScheme {
 
     /**
-     * Do tagging.
+     * Pointer (un)tagging VM operation.
      */
-    static public void doTagging() {
+    private static class PointerTaggingOperation extends VmOperation {
+
+        /**
+         * Cell pointer (un)tagger.
+         */
+        private static class CellPointerTagger extends CallbackCellVisitor {
+
+            /**
+             * A procedure to (un)tag a pointer.
+             */
+            private static class PointerIndexTagger extends PointerIndexVisitor {
+
+                /**
+                 * Action mode.
+                 */
+                private static ActionMode actionMode;
+
+                @Override
+                public void visit(Pointer pointer, int wordIndex) {
+                    Reference ref = pointer.getReference(wordIndex);
+                    Pointer refPointer = ref.toOrigin();
+
+                    if (refPointer.isZero())
+                        return;
+
+                    switch (actionMode) {
+                        case MaxSimPointerTaggingVerification:
+                            if (refPointer.isUntagged()) {
+                                TTY.println("WARNING: Untagged pointer found after tagging: [" +
+                                    pointer.plusWords(wordIndex).to0xHexString() + "] == " +
+                                    refPointer.to0xHexString() + " " + ref.toJava() + " classActor:" +
+                                    Layout.getHub(refPointer).classActor.toString());
+                                refPointer = setTagUsingObjectHub(refPointer);
+                            }
+                            break;
+                        case MaxSimPointerTagging:
+                            refPointer = setTagUsingObjectHub(refPointer);
+                            break;
+                        case MaxSimPointerUntaggingVerification:
+                            if (!refPointer.isUntagged()) {
+                                TTY.println("WARNING: Tagged pointer found after untagging: [" +
+                                    pointer.plusWords(wordIndex).to0xHexString() + "] == " +
+                                    refPointer.to0xHexString() + " " + ref.toJava() + " classActor:" +
+                                    Layout.getHub(refPointer).classActor.toString());
+                                refPointer = refPointer.tagClear();
+                            }
+                            break;
+                        case MaxSimPointerUntagging:
+                            refPointer = refPointer.tagClear();
+                            break;
+                        default:
+                            FatalError.unexpected("Unsupported tagging action mode.");
+                    }
+                    ref = Reference.fromOrigin(refPointer);
+                    pointer.setReference(wordIndex, ref);
+                }
+            }
+
+            /**
+             * Pointer index (un)tagger.
+             */
+            private static PointerIndexTagger pointerIndexTagger = new PointerIndexTagger();
+
+            /**
+             * Visit an array of references.
+             */
+            private void visitReferenceArray(Pointer origin) {
+                final int length = Layout.readArrayLength(origin);
+                int firstElementOffset = Layout.firstElementIndex();
+                for (int index = 0; index < length; index++) {
+                    pointerIndexTagger.visit(origin, firstElementOffset + index);
+                }
+            }
+
+            /**
+             * Empty callback.
+             */
+            protected boolean callback(Object object) {
+                return false;
+            }
+
+
+            /**
+             * Visits a cell for an object and (un)tags it.
+             */
+            public Pointer visitCell(Pointer cell) {
+                final Pointer origin = Layout.cellToOrigin(cell);
+                final Hub hub = Layout.getHub(origin);
+
+                // Update hub reference
+                pointerIndexTagger.visit(cell, Layout.hubIndex());
+
+                // Update referent
+                if (hub.isJLRReference) {
+                    int referentWordIndex = ClassRegistry.JLRReference_referent.offset() / Word.size();
+                    pointerIndexTagger.visit(origin, referentWordIndex);
+                }
+
+                // Update other references in an object
+                final SpecificLayout specificLayout = hub.specificLayout;
+                if (specificLayout.isTupleLayout()) {
+                    TupleReferenceMap.visitReferences(hub, origin, pointerIndexTagger);
+                    return cell.plus(hub.tupleSize);
+                }
+                if (specificLayout.isReferenceArrayLayout()) {
+                    visitReferenceArray(origin);
+                } else if (specificLayout.isHybridLayout()) {
+                    TupleReferenceMap.visitReferences(hub, origin, pointerIndexTagger);
+                }
+
+                return cell.plus(Layout.size(origin));
+            }
+
+            /**
+             * Sets pointer tagging (true) or untagging (false) operation mode.
+             */
+            void SetOperationMode(ActionMode actionMode) {
+                pointerIndexTagger.actionMode = actionMode;
+            }
+        }
+
+        /**
+         * Cell pointer (un)tagger.
+         */
+        private static final CellPointerTagger cellPointerTagger = new CellPointerTagger();
+
+        /**
+         * Roots (un)tagger.
+         */
+        private static final SequentialHeapRootsScanner heapRootsTagger =
+            new SequentialHeapRootsScanner(CellPointerTagger.pointerIndexTagger);
+
+        /**
+         * Pointer (un)tagging operation constructor.
+         */
+        protected PointerTaggingOperation() { super("PointerTaggingOperation", null, Mode.Safepoint);}
+
+        @Override
+        protected void doAfterFrozen(VmThread vmThread) {
+            Pointer tla = vmThread.tla();
+            final boolean threadWasInNative = LOWEST_ACTIVE_STACK_SLOT_ADDRESS.load(tla).isZero();
+
+            if (threadWasInNative) {
+                VmThreadLocal.prepareStackReferenceMap(tla);
+            } else {
+                final StackReferenceMapPreparer stackReferenceMapPreparer = vmThread.stackReferenceMapPreparer();
+                stackReferenceMapPreparer.completeStackReferenceMap(tla);
+            }
+        }
+
+        @Override
+        protected void doBeforeThawingThread(VmThread thread) {
+            LOWEST_ACTIVE_STACK_SLOT_ADDRESS.store3(thread.tla(), Address.zero());
+        }
+
+        @Override
+        protected void doIt() {
+            cellPointerTagger.SetOperationMode(cellPointerTagger.pointerIndexTagger.actionMode);
+            VmThreadLocal.prepareCurrentStackReferenceMap();
+            Heap.invokeGCCallbacks(Heap.GCCallbackPhase.BEFORE);
+            if (TraceMaxSimTagging) {
+                Log.println(cellPointerTagger.pointerIndexTagger.actionMode + " of heap roots.");
+            }
+            heapRootsTagger.run();
+            if (TraceMaxSimTagging) {
+                Log.println(cellPointerTagger.pointerIndexTagger.actionMode + " of code.");
+            }
+            Code.visitCells(cellPointerTagger, true);
+            if (TraceMaxSimTagging) {
+                Log.println(cellPointerTagger.pointerIndexTagger.actionMode + " of heap.");
+            }
+            VMConfiguration.vmConfig().heapScheme().walkHeap(cellPointerTagger);
+            Heap.invokeGCCallbacks(Heap.GCCallbackPhase.AFTER);
+        }
+
+        /**
+         * Do pointer (un)tagging.
+         */
+        protected void doTagging(ActionMode actionMode) {
+            cellPointerTagger.SetOperationMode(actionMode);
+            submit();
+        }
     }
 
     /**
-     * Do untagging.
+     * Pointer (un)tagging VM operation.
      */
-    static public void doUntagging() {
+    private static final PointerTaggingOperation pointerTaggingOperation = new PointerTaggingOperation();
+
+    /**
+     * Indicator that pointer (un)tagging is in progress
+     */
+    private static boolean isPointerTaggingInProgress = false;
+
+    /**
+     * Indicator that verification of pointer (un)tagging with correction should be performed (for debugging purposes).
+     */
+    private static final boolean doTaggingVerificationWithCorrection = false;
+
+    /**
+     * Action mode.
+     */
+    private enum ActionMode{
+        MaxSimPointerTagging,
+        MaxSimPointerUntagging,
+        MaxSimPointerTaggingVerification,
+        MaxSimPointerUntaggingVerification
+    }
+
+    /**
+     * Flag enabling/disabling tracing of MaxSim (un)tagging.
+     */
+    private static boolean TraceMaxSimTagging;
+    static {
+        VMOptions.addFieldOption("-XX:", "TraceMaxSimTagging", MaxSimTaggingScheme.class, "Trace MaxSim tagging.");
     }
 
     /**
@@ -63,6 +285,72 @@ public class MaxSimTaggingScheme {
     }
 
     /**
+     * Return true if (un)tagging is in progress, false otherwise.
+     */
+    @INLINE
+    public static boolean isTaggingInProgress() {
+        return isPointerTaggingInProgress;
+    }
+
+    /**
+     * Do tagging.
+     */
+    static public void doTagging() {
+        assert isPointerTaggingInProgress == false;
+        isPointerTaggingInProgress = true;
+        pointerTaggingOperation.doTagging(ActionMode.MaxSimPointerTagging);
+        isPointerTaggingInProgress = false;
+        if (doTaggingVerificationWithCorrection) {
+            doTaggingVerificationWithCorrection();
+        }
+    }
+
+    /**
+     * Do tagging verification.
+     */
+    static public void doTaggingVerificationWithCorrection() {
+        assert isPointerTaggingInProgress == false;
+        pointerTaggingOperation.doTagging(ActionMode.MaxSimPointerTaggingVerification);
+    }
+
+    /**
+     * Do untagging.
+     */
+    static public void doUntagging() {
+        assert isPointerTaggingInProgress == false;
+        isPointerTaggingInProgress = true;
+        pointerTaggingOperation.doTagging(ActionMode.MaxSimPointerUntagging);
+        isPointerTaggingInProgress = false;
+        if (doTaggingVerificationWithCorrection) {
+            doUntaggingVerificationWithCorrection();
+        }
+    }
+
+    /**
+     * Do untagging verification.
+     */
+    static public void doUntaggingVerificationWithCorrection() {
+        assert isPointerTaggingInProgress == false;
+        pointerTaggingOperation.doTagging(ActionMode.MaxSimPointerUntaggingVerification);
+    }
+
+    static final MaxSimHubTagUpdater maximHubTagUpdater = new MaxSimHubTagUpdater();
+
+    /**
+     * MaxSim hub tag updater.
+     */
+    static class MaxSimHubTagUpdater implements ClassActor.Closure {
+        public boolean doClass(ClassActor classActor) {
+            DynamicHub hub = classActor.dynamicHub();
+            if (hub == null) {
+                return true;
+            }
+            hub.setMaxSimHubTag(defineMaxSimHubTag(hub));
+            return true;
+        }
+    }
+
+    /**
      * Converts class ID to tag.
      */
     @INLINE
@@ -75,6 +363,17 @@ public class MaxSimTaggingScheme {
             return MaxSimInterface.PointerTag.TAG_UNDEFINED_GP_VALUE;
         }
         return (short) tag;
+    }
+
+    /**
+     * Converts tag to class ID.
+     */
+    @INLINE
+    public static int tagToClassID(short tag) {
+        if (MaxSimInterfaceHelpers.isGeneralPurposeTag(tag)) {
+            return UnsafeCast.asInt((short) (tag - MaxSimInterface.PointerTag.TAG_GP_LO_VALUE));
+        }
+        return ClassIDManager.NULL_CLASS_ID;
     }
 
     /**
