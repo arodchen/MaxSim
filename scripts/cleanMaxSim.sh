e5e25307c3da9211c7bedbd3c998b4607e41c68a
Andrey Rodchenko <andrey.rodchenko@manchester.ac.uk>
2017-03-31 10:04:06 +0100

    Added more scripts.

---
+++ scripts/cleanMaxSim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright 2017 Andrey Rodchenko, School of Computer Science, The University of Manchester
+# 
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.
+#                                                                          
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+. ./scripts/executeExitOnFail
+set -x
+executeExitOnFail ./scripts/cleanMaxine.sh
+executeExitOnFail ./scripts/cleanZSim.sh
